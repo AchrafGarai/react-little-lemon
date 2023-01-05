36fc5d6f6895e553b47c2d5f55cbf79ad566c5f4
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2023-01-05 15:10:00 +0100

    Updated logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <nav>
-      <img src="./little-lemon-logo.jpg" width={200} alt="Little Lemon Logo" />
+      <img src="./little-lemon-logo.png" width={200} alt="Little Lemon Logo" />
       <ul>
         <li><a href="/home">Homepage</a></li>        
         <li><a href="/about">About Us</a></li>
