108da3159441e0c02fd81041ba7fa87bf916b2a0
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2023-01-05 21:56:55 +0100

    Added booking confirmed component

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,13 @@
 import { Home, Booking } from '../pages'
 import { Route, Routes } from 'react-router-dom'
+import ConfirmedBooking from '../pages/ConfirmedBooking '
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
+      <Route path="/" element={<Home />}></Route>
       <Route path="/Booking" element={<Booking />}></Route>
+      <Route path="/confirmed-booking" element={<ConfirmedBooking />}></Route>
     </Routes>
   )
 }
